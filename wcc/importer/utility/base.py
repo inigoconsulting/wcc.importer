559ef8d5bf33509c1fb33001511e1c488e06a964
Izhar Firdaus <izhar@inigo-tech.com>
2013-03-08 12:29:08 +0800

    added importer for rawhtml contenttype for newsletters  (re #220)

--- wcc/importer/utility/base.py
+++ wcc/importer/utility/base.py
@@ -3,6 +3,8 @@ from wcc.importer.interfaces import IImporter
 from zope.container.interfaces import INameChooser
 import time
 import transaction
+from plone.dexterity.fti import DexterityFTI
+from plone.dexterity.utils import createContentInContainer
 
 class BaseImporter(grok.GlobalUtility):
     grok.implements(IImporter)
@@ -16,13 +18,24 @@ class BaseImporter(grok.GlobalUtility):
         raise NotImplementedError
 
     def _create_obj_for_title(self, container, portal_type, title):
+        fti = container.portal_types.get(portal_type)
+
+        if isinstance(fti, DexterityFTI):
+            obj = createContentInContainer(container, portal_type, title=title) 
+            transaction.savepoint(optimistic=True)
+            return obj.id
+
         oid = container.invokeFactory(type_name=portal_type, id=time.time())
+
         transaction.savepoint(optimistic=True)
+
         obj = container._getOb(oid)
         # set id
         oid = INameChooser(container).chooseName(title, obj)
+
         obj.setId(oid)
         obj.setTitle(title)
+
         return oid
 
 
---
+++ wcc/importer/utility/rawhtmlimporter.py
@@ -0,0 +1,28 @@
+from five import grok
+import dateutil.parser
+from zope.annotation.interfaces import IAnnotations
+from persistent.dict import PersistentDict
+import logging
+logger = logging.getLogger("wcc.importer")
+from base64 import b64decode
+from wcc.importer.utility.base import BaseImporter
+
+class RawHTMLImporter(BaseImporter):
+    grok.name('wcc.importer.rawhtmlimporter')
+
+    def _factory(self, container, entry):
+        logger.info("Creating RAWHTML Item : %s" % entry['title'])
+
+        oid = self._create_obj_for_title(container, 'wcc.rawhtml.rawhtml', entry['title'])
+        obj = container._getOb(oid)
+
+        obj.raw_html = entry['raw_html']
+
+        # remember original url
+        anno = IAnnotations(obj)
+        anno.setdefault('wcc.metadata', PersistentDict())
+        anno['wcc.metadata']['original_url'] = entry['orig_url']
+        anno['wcc.metadata']['lang_urls'] = entry['lang_urls']
+        obj.reindexObject()
+
+        logger.info("Created %s" % obj.absolute_url())
